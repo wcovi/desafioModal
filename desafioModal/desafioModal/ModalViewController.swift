0c5502011562323afbbe8983976350197dd7ba08
wcovi <wcovi@yahoo.com.br>
2023-02-04 17:52:50 -0300

    feature: project commit

---
+++ desafioModal/desafioModal/ModalViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ModalViewController.swift
+//  desafioModal
+//
+//  Created by Wolfgang Florian Covi on 02/02/23.
+//
+
+import UIKit
+
+class ModalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func dismissButton(_ sender: Any) {
+        dismiss(animated: true)
+    }
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
